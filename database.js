fdd4b76b380214e2bc0f5f754fba74890de644a5
aliardatrcn <76741492+aliardatrcn@users.noreply.github.com>
2022-03-29 10:05:23 +0300

    Update database.js

--- database.js
+++ database.js
@@ -14,8 +14,7 @@ var options = {
 
 
 function getAllLocations(cb) {
-      DATABASE_PGB.any('SELECT name, surname, start, finish, duration, ST_X(geometry) as longitude, ST_Y(geometry) as latitude, type FROM period 
-WHERE time > NOW() - interval'1 minutes'')
+      DATABASE_PGB.any('SELECT name, surname, start, finish, duration, ST_X(geometry) as longitude, ST_Y(geometry) as latitude, type FROM period')
       .then(function (data) {
          cb(null, data);})
        .catch(function (err) {
